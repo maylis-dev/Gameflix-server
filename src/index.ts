b93483466503a683c624bf453b4d20d9d208434b
maylis imperiali <imp.mayli@gmail.com>
2026-06-15 10:48:40 +0200

    serverfinished

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import gameRoute from "./routes/games.routes";
+import { prisma } from "./prisma/client";
 
 const app = express();
 
@@ -10,8 +11,16 @@ app.use(express.json());
 
 app.use("/games", gameRoute);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the WorkUp API!" });
+app.get("/", async (req, res) => {
+  try {
+    const games = await prisma.game.findMany();
+    res.json(games);
+  } catch (error) {
+    console.log("GET GAMES ERROR:", error);
+    res.status(500).json({
+      error: "Failed to fetch games",
+    });
+  }
 });
 
 app.listen(5005, () => {
