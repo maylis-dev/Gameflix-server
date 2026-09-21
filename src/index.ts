b491e75b69938b8e7cefa3284330d4430d3def0c
maylis imperiali <imp.mayli@gmail.com>
2026-09-21 10:27:07 +0200

    ver

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,19 @@ app.use(express.json());
 app.use("/games", gameRoute);
 
 app.get("/", async (req, res) => {
-  try {
-    const games = await prisma.game.findMany();
-    res.json(games);
-  } catch (error) {
-    console.log("GET GAMES ERROR:", error);
-    res.status(500).json({
-      error: "Failed to fetch games",
-    });
-  }
+	try {
+		const games = await prisma.game.findMany();
+		res.json(games);
+	} catch (error) {
+		console.log("GET GAMES ERROR:", error);
+		res.status(500).json({
+			error: "Failed to fetch games",
+		});
+	}
 });
 
-app.listen(5005, () => {
-  console.log("Server is running on http://localhost:5005");
-});
+// app.listen(5005, () => {
+//   console.log("Server is running on http://localhost:5005");
+// });
 
 export default app;
