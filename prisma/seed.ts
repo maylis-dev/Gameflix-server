05683a1bc6519d2c40535ed92abe9e876496a753
maylis imperiali <imp.mayli@gmail.com>
2026-09-22 18:22:40 +0200

    update game seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+	await prisma.game.deleteMany();
+
 	await prisma.game.createMany({
 		data: [
 			// {
